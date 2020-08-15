35ec13ad9ad0f5ed4f6d71621a3d02bb28eef721
kirimaru <nainaistar@gmail.com>
2020-08-15 13:45:59 +0000

    7 java

---
+++ .leetcode/7.reverse-integer.java
@@ -0,0 +1,29 @@
+/**
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ */
+// @lc code=start
+class Solution {
+    public int reverse(int x) {
+        String str = String.valueOf(x);
+
+        String rtn = "";
+        if (x < 0){
+            str = str.substring(1, str.length());
+            rtn = "-";
+        }
+
+        for (int i=str.length();i > 0; i--){
+            rtn += str.substring(i - 1, i);
+        }
+        try {
+            return Integer.parseInt(rtn);
+        } catch(Exception e){
+            return 0;
+        }
+        
+    }
+}
+// @lc code=end
+
