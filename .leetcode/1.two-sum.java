d41df6ce33fd3f53083b4c95a56fbd43348988a7
kirimaru <nainaistar@gmail.com>
2020-08-15 05:36:06 +0000

    [Add] leetcodeのファイル追加

---
+++ .leetcode/1.two-sum.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        List<Integer> listInt = new ArrayList<>();
+      
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+               if (nums[i] + nums[j] == target) {
+                    listInt.add(i);
+                    listInt.add(j);
+               }
+            }
+        }  
+
+        int[] rtn = new int[listInt.size()];
+        int i = 0;
+        for (var a: listInt){
+            rtn[i] = a;
+            i++;
+        }
+        
+       return rtn;
+    }
+}
+// @lc code=end
+
