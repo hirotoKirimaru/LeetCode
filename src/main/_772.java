cf4f48983ffa229d9d993696b8cd9d982ee16412
kirimaru <nainaistar@gmail.com>
2020-11-13 10:04:47 +0000

    通ったテストをこちらでやる

--- src/main/_772.java
+++ src/main/_772.java
@@ -10,5 +10,31 @@
  */
 class _772 {
   public boolean isPalindrome(ListNode head) {
+    if (Objects.isNull(head)) {
+      return true;
+    }
+
+    ArrayList<Integer> list = new ArrayList<>();
+    boolean loop = true;
+    while (loop) {
+      list.add(head.val);
+      head = head.next;
+
+      if (Objects.isNull(head)) {
+        loop = false;
+        break;
+      }
+    }
+    boolean actual = true;
+    for (int i = 0; i <= list.size() / 2; i++) {
+
+      int compareA = list.get(i);
+      int compareB = list.get(list.size() - 1 - i);
+      if (compareA != compareB) {
+
+        return false;
+      }
+    }
+    return true;
   }
 }
