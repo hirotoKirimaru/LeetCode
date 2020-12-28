9a829b4af67076b5f602d000b746ea5078d6ccfe
nainaistar <nainaistar@gmail.com>
2020-12-28 17:59:17 +0900

    [Add] WIP: test追加

--- src/test/java/com/example/demo/_2Tests.java
+++ src/test/java/com/example/demo/_2Tests.java
@@ -49,9 +49,50 @@ class _2Tests {
 
   }
 
-  @Disabled("桁あふれをいい感じにする")
   @Test
   void test_03() {
+    _2.ListNode param1_1 = new _2.ListNode(9);
+    _2.ListNode param1_2 = new _2.ListNode(9);
+    _2.ListNode param1_3 = new _2.ListNode(9);
+    _2.ListNode param1_4 = new _2.ListNode(9);
+    _2.ListNode param1_5 = new _2.ListNode(9);
+    _2.ListNode param1_6 = new _2.ListNode(9);
+    _2.ListNode param1_7 = new _2.ListNode(9);
+
+    param1_1.next = param1_2;
+    param1_2.next = param1_3;
+    param1_3.next = param1_4;
+    param1_4.next = param1_5;
+    param1_5.next = param1_6;
+    param1_6.next = param1_7;
+
+    _2.ListNode param2_1 = new _2.ListNode(9);
+    _2.ListNode param2_2 = new _2.ListNode(9);
+    _2.ListNode param2_3 = new _2.ListNode(9);
+    _2.ListNode param2_4 = new _2.ListNode(9);
+
+    param2_1.next = param2_2;
+    param2_2.next = param2_3;
+    param2_3.next = param2_4;
+
+    _2.ListNode actual = target.addTwoNumbers(param1_1, param2_1);
+
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(actual.val).isEqualTo(8);
+    softly.assertThat(actual.next.val).isEqualTo(9);
+    softly.assertThat(actual.next.next.val).isEqualTo(9);
+    softly.assertThat(actual.next.next.next.val).isEqualTo(9);
+    softly.assertThat(actual.next.next.next.next.val).isEqualTo(0);
+    softly.assertThat(actual.next.next.next.next.next.val).isEqualTo(0);
+    softly.assertThat(actual.next.next.next.next.next.next.val).isEqualTo(0);
+    softly.assertThat(actual.next.next.next.next.next.next.next.val).isEqualTo(1);
+    softly.assertAll();
+
+  }
+
+  @Disabled("桁あふれをいい感じにする")
+  @Test
+  void test_04() {
     _2.ListNode param1_1 = new _2.ListNode(9);
 
     _2.ListNode param2_1 = new _2.ListNode(1);
