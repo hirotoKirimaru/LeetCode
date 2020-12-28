dc3a73a6cbb9ba7a9a4b3e172beb8902423c1322
nainaistar <nainaistar@gmail.com>
2020-12-28 17:12:40 +0900

    [Add] _2の桁あふれがまだ解決できてない

---
+++ src/main/java/com/example/demo/_2.java
@@ -0,0 +1,103 @@
+package com.example.demo;//You are given two non-empty linked lists representing two non-negative integer
+//s. The digits are stored in reverse order, and each of their nodes contains a si
+//ngle digit. Add the two numbers and return the sum as a linked list.
+//
+// You may assume the two numbers do not contain any leading zero, except the nu
+//mber 0 itself.
+//
+//
+// Example 1:
+//
+//
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+//
+//
+// Example 2:
+//
+//
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+//
+//
+// Example 3:
+//
+//
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in each linked list is in the range [1, 100].
+// 0 <= Node.val <= 9
+// It is guaranteed that the list represents a number that does not have leading
+// zeros.
+//
+// Related Topics Linked List Math
+// 👍 10101 👎 2534
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class _2 {
+  @Setter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+      this.val = val;
+    }
+  }
+
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    StringBuilder l1Number = new StringBuilder();
+    while (l1 != null) {
+      l1Number.insert(0, l1.val);
+      l1 = l1.next;
+    }
+
+    StringBuilder l2Number = new StringBuilder();
+    while (l2 != null) {
+      l2Number.insert(0, l2.val);
+      l2 = l2.next;
+    }
+
+    var sum = Long.parseLong(l1Number.toString()) + Long.parseLong(l2Number.toString());
+
+    String sumStr = String.valueOf(sum);
+
+    ListNode rtn = null;
+    while (!sumStr.equals("")) {
+      rtn = new ListNode(Integer.parseInt(sumStr.substring(0, 1)), rtn);
+      sumStr = sumStr.substring(1);
+    }
+
+
+    return rtn;
+  }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/demo/_2Tests.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class _2Tests {
+  _2 target = new _2();
+
+  @Test
+  void test_01() {
+    _2.ListNode param1_1 = new _2.ListNode(2);
+    _2.ListNode param1_2 = new _2.ListNode(4);
+    _2.ListNode param1_3 = new _2.ListNode(3);
+
+    param1_1.next = param1_2;
+    param1_2.next = param1_3;
+
+    _2.ListNode param2_1 = new _2.ListNode(5);
+    _2.ListNode param2_2 = new _2.ListNode(6);
+    _2.ListNode param2_3 = new _2.ListNode(4);
+
+    param2_1.next = param2_2;
+    param2_2.next = param2_3;
+
+    _2.ListNode actual = target.addTwoNumbers(param1_1, param2_1);
+
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(actual.val).isEqualTo(7);
+    softly.assertThat(actual.next.val).isEqualTo(0);
+    softly.assertThat(actual.next.next.val).isEqualTo(8);
+    softly.assertAll();
+
+  }
+
+  @Test
+  void test_02() {
+    _2.ListNode param1_1 = new _2.ListNode(0);
+
+    _2.ListNode param2_1 = new _2.ListNode(5);
+
+    _2.ListNode actual = target.addTwoNumbers(param1_1, param2_1);
+
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(actual.val).isEqualTo(0);
+    softly.assertAll();
+
+  }
+
+  @Test
+  void test_03() {
+    _2.ListNode param1_1 = new _2.ListNode(9);
+
+    _2.ListNode param2_1 = new _2.ListNode(1);
+    _2.ListNode param2_2 = new _2.ListNode(9);
+    _2.ListNode param2_3 = new _2.ListNode(9);
+    _2.ListNode param2_4 = new _2.ListNode(9);
+    _2.ListNode param2_5 = new _2.ListNode(9);
+    _2.ListNode param2_6 = new _2.ListNode(9);
+    _2.ListNode param2_7 = new _2.ListNode(9);
+    _2.ListNode param2_8 = new _2.ListNode(9);
+    _2.ListNode param2_9 = new _2.ListNode(9);
+    _2.ListNode param2_10 = new _2.ListNode(9);
+
+    param2_1.next = param2_2;
+    param2_2.next = param2_3;
+    param2_3.next = param2_4;
+    param2_4.next = param2_5;
+    param2_5.next = param2_6;
+    param2_6.next = param2_7;
+    param2_7.next = param2_8;
+    param2_8.next = param2_9;
+    param2_9.next = param2_10;
+
+
+    try {
+      _2.ListNode actual = target.addTwoNumbers(param1_1, param2_1);
+    } catch (Exception e) {
+      fail();
+    }
+  }
+
+}
