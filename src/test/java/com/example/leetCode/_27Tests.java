58c6ef0ad8c1c23cbc8abd5bdbf7e10c70e514f1
nainaistar <nainaistar@gmail.com>
2021-01-25 22:12:50 +0900

    [Add] _26 Baby step

---
+++ src/test/java/com/example/leetCode/_27Tests.java
@@ -0,0 +1,21 @@
+package com.example.leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _27Tests {
+  _27 target = new _27();
+
+  @Test
+  void test_01(){
+    int[] param1 = new int[]{ 3,2,2,3};
+    int i = target.removeElement(param1, 3);
+
+    assertThat(i).isEqualTo(2);
+    assertThat(param1[0]).isEqualTo(2);
+    assertThat(param1[1]).isEqualTo(2);
+  }
+
+}
