2e6069979c6c44e204ab687a1443274cb9118066
nainaistar <nainaistar@gmail.com>
2021-02-07 23:24:58 +0900

    [Add] 6 BabyStep

---
+++ src/test/java/com/example/leetCode/_6Tests.java
@@ -0,0 +1,15 @@
+package com.example.leetCode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _6Tests {
+  _6 target = new _6();
+
+  @org.junit.jupiter.api.Test
+  void test_01() {
+    assertThat(target.convert("PAYPALISHIRING", 3)).isEqualTo("PAHNAPLSIIGYIR");
+
+
+  }
+}
