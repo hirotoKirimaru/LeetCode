380b1176da659a75102cf8f9681ae96554cf0739
nainaistar <nainaistar@gmail.com>
2021-02-18 19:08:41 +0900

    [Add] _69 ライブラリ呼ぶだけ

---
+++ src/main/java/com/example/leetCode/_69.java
@@ -0,0 +1,38 @@
+package com.example.leetCode;//Given a non-negative integer x, compute and return the square root of x.
+//
+// Since the return type is an integer, the decimal digits are truncated, and on
+//ly the integer part of the result is returned.
+//
+//
+// Example 1:
+//
+//
+//Input: x = 4
+//Output: 2
+//
+//
+// Example 2:
+//
+//
+//Input: x = 8
+//Output: 2
+//Explanation: The square root of 8 is 2.82842..., and since the decimal part is
+// truncated, 2 is returned.
+//
+//
+// Constraints:
+//
+//
+// 0 <= x <= 231 - 1
+//
+// Related Topics Math Binary Search
+// 👍 1799 👎 2229
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class _69{
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/leetCode/_69Tests.java
@@ -0,0 +1,12 @@
+package com.example.leetCode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _69Tests {
+  _69 target = new _69();
+
+  @Test
+  void test_01() {
+
+  }
+}
