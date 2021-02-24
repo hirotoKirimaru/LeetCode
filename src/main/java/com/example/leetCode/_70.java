4b91f2778fe9fc8a5e28d0cdb957a2db3b86fe2f
nainaistar <nainaistar@gmail.com>
2021-02-24 21:04:30 +0900

    [Add] _70 コミットだけする

---
+++ src/main/java/com/example/leetCode/_70.java
@@ -0,0 +1,44 @@
+package com.example.leetCode;//You are climbing a staircase. It takes n steps to reach the top.
+//
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can yo
+//u climb to the top?
+//
+//
+// Example 1:
+//
+//
+//Input: n = 2
+//Output: 2
+//Explanation: There are two ways to climb to the top.
+//1. 1 step + 1 step
+//2. 2 steps
+//
+//
+// Example 2:
+//
+//
+//Input: n = 3
+//Output: 3
+//Explanation: There are three ways to climb to the top.
+//1. 1 step + 1 step + 1 step
+//2. 1 step + 2 steps
+//3. 2 steps + 1 step
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= n <= 45
+//
+// Related Topics Dynamic Programming
+// 👍 5965 👎 187
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class _70 {
+  public int climbStairs(int n) {
+    return n;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/leetCode/_70Tests.java
@@ -0,0 +1,12 @@
+package com.example.leetCode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _70Tests {
+  _70 target = new _70();
+
+  @Test
+  void test_01() {
+
+  }
+}
