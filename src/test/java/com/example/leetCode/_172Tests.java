dbcd3ed40b09dac521dd9ac1c76171a6c0e09206
nainaistar <nainaistar@gmail.com>
2021-03-26 16:22:56 +0900

    [Add] _172 BabyStep

---
+++ src/test/java/com/example/leetCode/_172Tests.java
@@ -0,0 +1,32 @@
+package com.example.leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _172Tests {
+  _172 target = new _172();
+
+  @Test
+  void test_01() {
+    assertThat(
+        target.trailingZeroes(3)
+    ).isEqualTo(0);
+  }
+
+  @Test
+  void test_02() {
+    assertThat(
+        target.trailingZeroes(5)
+    ).isEqualTo(1);
+  }
+
+  @Test
+  void test_03() {
+    assertThat(
+        target.trailingZeroes(0)
+    ).isEqualTo(0);
+  }
+
+}
