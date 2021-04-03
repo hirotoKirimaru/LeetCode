0eef75bf2a0c6daaa787079f54d0debb59227c57
nainaistar <nainaistar@gmail.com>
2021-04-03 17:05:29 +0900

    [Add] _100 BabyStep

---
+++ src/main/java/com/example/leetCode/_485.java
@@ -0,0 +1,30 @@
+package com.example.leetCode;
+//Given a binary array, find the maximum number of consecutive 1s in this array.
+//
+//
+// Example 1:
+//
+//Input: [1,1,0,1,1,1]
+//Output: 3
+//Explanation: The first two digits or the last three digits are consecutive 1s.
+//
+//    The maximum number of consecutive 1s is 3.
+//
+//
+//
+// Note:
+//
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+//
+// Related Topics Array
+// 👍 1176 👎 382
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class _485 {
+  public int findMaxConsecutiveOnes(int[] nums) {
+    return 3;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/leetCode/_485Tests.java
@@ -0,0 +1,17 @@
+package com.example.leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _485Tests {
+  _485 target = new _485();
+
+  @Test
+  void test_01() {
+    assertThat(
+        target.findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1})
+    ).isEqualTo(3);
+  }
+}
