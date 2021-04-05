442585b7d472b27cddb280c774de8d4d5721257c
nainaistar <nainaistar@gmail.com>
2021-04-05 17:57:42 +0900

    [Add] _101 よくわからなかった

---
+++ src/test/java/com/example/leetCode/_101Tests.java
@@ -0,0 +1,14 @@
+package com.example.leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _101Tests {
+  _101 target = new _101();
+
+  @Test
+  void test_01() {
+
+  }
+}
