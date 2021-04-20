2f39d78e6947d434373b9954e1fbdc6409aa9dd8
nainaistar <nainaistar@gmail.com>
2021-04-20 16:05:00 +0900

    [Add] _1832 BabyStep

---
+++ src/main/java/com/example/leetCode/_1832.java
@@ -0,0 +1,42 @@
+package com.example.leetCode;
+//A pangram is a sentence where every letter of the English alphabet appears at
+//least once.
+//
+// Given a string sentence containing only lowercase English letters, return tru
+//e if sentence is a pangram, or false otherwise.
+//
+//
+// Example 1:
+//
+//
+//Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+//Output: true
+//Explanation: sentence contains at least one of every letter of the English alp
+//habet.
+//
+//
+// Example 2:
+//
+//
+//Input: sentence = "leetcode"
+//Output: false
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= sentence.length <= 1000
+// sentence consists of lowercase English letters.
+//
+// Related Topics String
+// 👍 54 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class _1832 {
+  public boolean checkIfPangram(String sentence) {
+    return true;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/leetCode/_1832Tests.java
@@ -0,0 +1,17 @@
+package com.example.leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _1832Tests {
+  _1832 target = new _1832();
+
+  @Test
+  void test_01() {
+    assertThat(
+        target.checkIfPangram("thequickbrownfoxjumpsoverthelazydog")
+    ).isEqualTo(true);
+  }
+}
