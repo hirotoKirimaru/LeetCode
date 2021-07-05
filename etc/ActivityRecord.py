ee4df3a24d65e7a3b9cd0abd9d1c3c0fb7bf1218
kirimaru <nainaistar@gmail.com>
2021-07-05 10:51:19 +0000

    [Add] 処理追加

--- etc/ActivityRecord.py
+++ etc/ActivityRecord.py
@@ -1,6 +1,7 @@
 import json
 import math
 import sys
+import datetime
 
 json_days = open('wakatime.json', 'r')
 json_load = json.load(json_days)
@@ -9,7 +10,15 @@ if len(sys.argv) < 3:
     print('パラメータが必要です')
     print('例')
     print('python ファイル名 開始日 終了日（開始日は含むが終了日は含まない）')
-    print('python ActivitiRecord.py 2021-02-01 2021-03-01')
+
+    now = datetime.datetime.now()
+    start_date = f"{now:%Y-%m-%d}"
+    end_date = f"{now:%Y-%m-%d}"
+
+    # print('python ActivityRecord.py 2021-02-01 2021-03-01')
+    # TODO: いい感じにしたい
+    print('python ActivityRecord.py {0} {1}'.format(start_date, end_date))
+    
     exit()
 
 start_date = sys.argv[1]
