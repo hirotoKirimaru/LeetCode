49cbe682447f00e7eea6361bfd78f4abd79196ff
水上 皓登 <nainaistar@gmail.com>
2021-08-22 10:57:36 +0900

    add: 1952 BabyStep

---
+++ src/main/java/com/example/leetCode/_1952.java
@@ -0,0 +1,41 @@
+package com.example.leetCode;
+//Given an integer n, return true if n has exactly three positive divisors.
+//Otherwise, return false.
+//
+// An integer m is a divisor of n if there exists an integer k such that n = k *
+// m.
+//
+//
+// Example 1:
+//
+//
+//Input: n = 2
+//Output: false
+//Explantion: 2 has only two divisors: 1 and 2.
+//
+//
+// Example 2:
+//
+//
+//Input: n = 4
+//Output: true
+//Explantion: 4 has three divisors: 1, 2, and 4.
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= n <= 10⁴
+//
+// Related Topics Math 👍 105 👎 9
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class _1952 {
+
+  public boolean isThree(int n) {
+    return false;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/test/java/com/example/leetCode/_1952Tests.java
@@ -0,0 +1,17 @@
+package com.example.leetCode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class _1952Tests {
+  _1952 target = new _1952();
+
+  @Test
+  void test_01() {
+    assertThat(
+        target.isThree(2)
+    ).isFalse();
+  }
+}
